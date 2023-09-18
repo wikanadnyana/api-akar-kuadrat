611901e01da71e551424f0a804f74b1e8871f5c9
wikanadnyana <wikanadnyana44@gmail.com>
2023-09-18 21:14:48 +0800

    [FIX] adding cors

--- main.go
+++ main.go
@@ -4,12 +4,18 @@ import (
 	"api-akar-kuadrat/controllers/calculatecontroller"
 	"api-akar-kuadrat/models"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func main()  {
 	router:= gin.Default()
 	models.ConnectDatabase()
+
+	config := cors.DefaultConfig()
+	config.AllowOrigins = []string{"*"}
+	config.AllowMethods = []string{"GET", "POST", "PUT", "DELETE"}
+	router.Use(cors.New(config))
 
 	router.POST("/api/calculate", calculatecontroller.Create)
 
