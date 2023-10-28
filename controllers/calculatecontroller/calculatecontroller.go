2d8ffba0bfc3140b8261a264deacedeca72c2333
wikanadnyana <wikanadnyana44@gmail.com>
2023-10-28 12:14:15 +0800

    fix: edit comment

--- controllers/calculatecontroller/calculatecontroller.go
+++ controllers/calculatecontroller/calculatecontroller.go
@@ -13,45 +13,52 @@ func Create(context *gin.Context) {
 		Number float64 `json:"number"`
 	}
 
+	// Proses input data (NODES 1)
 	if err := context.ShouldBindJSON(&inputData); err != nil {
 		context.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"message": err.Error()})
 		return
 	}
-	// validation
+	// Proses validasi angka yang invalid (NODES 2)
 	if inputData.Number < 0 {
+		// Proses ketika nilai input bernilai false (NODES 3) 
 		context.JSON(http.StatusBadRequest, gin.H{"error": "Value must be a positive number"})
+		return
+	}else if inputData.Number > 9223372036854775807 {
+		context.JSON(http.StatusBadRequest, gin.H{"error": "Angka melebihi kapasitas"})
 		return
 	}
 
+	//Proses ketika nilai input bernilai True (NODES 4)
 	var hasilAkarKuadrat float64
 
 	if inputData.Number == 0 {
 		hasilAkarKuadrat = 0
 	}else{
 		hasilAkarKuadrat = hitungakar(inputData.Number)
-	}
+	}	
 	
 
 
-	if hasilAkarKuadrat == -92233720368547.77{
-		context.JSON(http.StatusBadRequest, gin.H{"error": "Angka melebihi kapasitas"})
-		return
-	}
+	// if hasilAkarKuadrat == -92233720368547.77{
+	// 	context.JSON(http.StatusBadRequest, gin.H{"error": "Angka melebihi kapasitas"})
+	// 	return
+	// }
 
 
 	calculation := models.Calculate{
 		Number:      inputData.Number,
 		Akarkuadrat: hasilAkarKuadrat,
 	}
-
+	// Proses penyimpanan hasil di database (NODES 6)
 	if err := models.DB.Create(&calculation).Error; err != nil {
 		context.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"message": "Failed to create calculation record"})
 		return
 	}
-
+	// Hasil proses perhitungan dan response JSON (NODES 7)
 	context.JSON(http.StatusOK, gin.H{"calculation": calculation.Akarkuadrat})
 }
 
+//Proses perhitungan akar kuadrat (NODES 5)
 func hitungakar(number float64) float64 {
 	var squareroot float64 = number / 2
 	var temp float64
