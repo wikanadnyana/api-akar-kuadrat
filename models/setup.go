199f30ef34e56ac83880797e19e298fa4b579bc7
AUDREY TILANOV <tilanovaudrey@gmail.com>
2023-10-31 04:21:23 +0800

    fix db conn - cloud sql

--- models/setup.go
+++ models/setup.go
@@ -9,7 +9,7 @@ var DB *gorm.DB
 
 func ConnectDatabase() {
 	// database, err := gorm.Open(mysql.Open("projec27_akar:akarakarakar12@tcp(103.123.62.41:3306)/projec27_akarkuadrat"))
-	database, err := gorm.Open(mysql.Open("root:@tcp(localhost:3306)/tugas_ppl_akarkuadrat2"))
+	database, err := gorm.Open(mysql.Open("root:appengine-laravel@cloudsqlconn(localhost:3306)/appengine-laravel?parseTime=true"))
 
 	if err != nil {
 		panic(err)
