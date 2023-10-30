bd9522e278b1185464f3122323225d13a0dad6bf
AUDREY TILANOV <tilanovaudrey@gmail.com>
2023-10-31 04:35:12 +0800

    tcp conn

--- models/setup.go
+++ models/setup.go
@@ -9,7 +9,8 @@ var DB *gorm.DB
 
 func ConnectDatabase() {
 	// database, err := gorm.Open(mysql.Open("projec27_akar:akarakarakar12@tcp(103.123.62.41:3306)/projec27_akarkuadrat"))
-	database, err := gorm.Open(mysql.Open("root:appengine-laravel@cloudsqlconn(localhost:3306)/appengine-laravel?parseTime=true"))
+	// database, err := gorm.Open(mysql.Open("root:appengine-laravel@unix('/cloudsql/sacred-highway-400710:asia-southeast2:appengine-laravel')/appengine-laravel?parseTime=true"))
+	database, err := gorm.Open(mysql.Open("root:appengine-laravel@tcp(localhost:3306)/appengine-laravel?parseTime=true"))
 
 	if err != nil {
 		panic(err)
